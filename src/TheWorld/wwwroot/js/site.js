71b6ab852ae3ee5439e8e3151193ed1991e08adb
quintelab <quintelab@gmail.com>
2016-05-21 11:12:54 -0300

    Add bower and jquery to the project

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,16 @@
+//site.js
+
+(function () {
+    var $sidebarAndWrapper = $("#sidebar,#wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+
+        if ($sidebarAndWrapper.hasClass("hide-sidebar")) {
+            $(this).text("Show Sidebar");
+        } else {
+            $(this).text("Hide Sidebar");
+        }
+    });
+
+})();
